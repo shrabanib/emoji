b5e826820d4a378067a7e4df9f7588dd549384c5
Shrabani Bhowmick <shrabanibhowmick@yahoo.com>
2022-01-04 20:01:48 -0500

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.e52748fab4a99ea8769d2e33befa827c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b30f70d03783f86dd3075dc8ba3165",
+    "url": "/emoji/index.html"
+  },
+  {
+    "revision": "229edf68f35bb0fdb152",
+    "url": "/emoji/static/js/2.114b6fed.chunk.js"
+  },
+  {
+    "revision": "50e82fdc7c46a18f5625",
+    "url": "/emoji/static/js/main.30827037.chunk.js"
+  },
+  {
+    "revision": "1b926739bce4502cfa3e",
+    "url": "/emoji/static/js/runtime-main.8f95762c.js"
+  }
+]);
